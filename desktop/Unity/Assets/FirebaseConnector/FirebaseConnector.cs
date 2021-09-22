27a9065bb23a816b22f27a81888ac055deb0aae9
npgy <me@np.gy>
2021-09-22 14:06:10 -0400

    NMP: Added component base files

---
+++ desktop/Unity/Assets/FirebaseConnector/FirebaseConnector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FirebaseConnectorComponent
+{
+    //Nick Preston
+    public class FirebaseConnector : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
+

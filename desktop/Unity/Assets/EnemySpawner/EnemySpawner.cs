3cf09b248dd4940b511951f84f9d98350ce62a91
tyler23456 <tsa4992@uncw.edu>
2021-09-21 18:02:48 -0700

    Updated Unity Version

---
+++ desktop/Unity/Assets/EnemySpawner/EnemySpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnemySpawnerComponent
+{
+    //Tyler Anderson
+    public class EnemySpawner : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}

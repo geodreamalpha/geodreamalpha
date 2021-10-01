58a9865207a75bdc62b9427033905190e0e35ebe
jsa5664 <47936036+jsa5664@users.noreply.github.com>
2021-09-30 21:25:43 -0400

    Added component API and data models for PlayerMovement class.

--- desktop/Unity/Assets/PlayerMovement/Player.cs
+++ desktop/Unity/Assets/PlayerMovement/Player.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace PlayerMovementComponent
 {
-     public class Player
+    public class Player
     {
-        Stats playerStats = new Stats(); 
+        Stats playerStats = new Stats();
 
+        /// <summary>
+        /// Get current player stats. 
+        /// </summary>
+        /// <returns>Current player stats object.</returns>
         public Stats GetPlayerStats()
         {
-            return playerStats; 
+            return playerStats;
         }
     }
 }

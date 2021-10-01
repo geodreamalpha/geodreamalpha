fc921d0b4c79be3a890428a66e455eb364923ad0
Nick <me@np.gy>
2021-09-30 23:07:35 -0400

    Defined user data model

---
+++ desktop/Unity/Assets/UserDataModel/User.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UserDataModel
+{
+    //Nick Preston
+    public class User : MonoBehaviour
+    {
+        string email;
+        object stats;
+    }
+}
+

0bddf147841bb9f386e479dde39ee72d349abfab
Aaron Schwartz <ams1347@uncw.edu>
2021-10-04 14:30:58 -0400

    Added data model classes to Android project

--- mobile/companion_app_project/app/src/main/java/com/hfad/gacompanion/UserManager.java
+++ mobile/companion_app_project/app/src/main/java/com/hfad/gacompanion/UserManager.java
@@ -1,13 +1,44 @@
 package com.hfad.gacompanion;
+
+import android.view.View;
+
 //Aaron Schwartz
 public class UserManager {
 
-    public UserManager(){
+    private String username;
+    private String password;
+    private Companion userCompanion;
 
+    public UserManager(String username, String password){
+        this.username = username;
+        this.password = password;
     }
 
     public String hello(){
         return "Hello from UserManager!";
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Companion getUserCompanion() {
+        return userCompanion;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserCompanion(Companion userCompanion) {
+        this.userCompanion = userCompanion;
+    }
 }

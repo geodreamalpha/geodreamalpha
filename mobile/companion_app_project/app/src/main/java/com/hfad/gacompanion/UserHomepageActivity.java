92c1cfc9c42fa926475a7342279f67babfc69414
Aaron Schwartz <ams1347@uncw.edu>
2021-10-14 17:06:39 -0400

    Added Firebase authentication functionality to log in screen
    Also added new "about" page and made the log-in screen the main launcher activity

--- mobile/companion_app_project/app/src/main/java/com/hfad/gacompanion/UserHomepageActivity.java
+++ mobile/companion_app_project/app/src/main/java/com/hfad/gacompanion/UserHomepageActivity.java
@@ -2,13 +2,28 @@ package com.hfad.gacompanion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 //Aaron Schwartz
 public class UserHomepageActivity extends AppCompatActivity {
+
+
+    public static final String FIRST_TIME = "first time";
+    private static final String TAG = "UserHomepageActivity";
+    private FirebaseAuth mAuth;
+    private boolean first;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_homepage);
+
+        Intent createdIntent = getIntent();
+        this.first = createdIntent.getBooleanExtra(FIRST_TIME, false);
+        Log.d(TAG, "first value" + first);
     }
 }

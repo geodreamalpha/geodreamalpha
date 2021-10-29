00570a9d444744d0b211214cd9d0f10ab587ef70
Nick <me@np.gy>
2021-10-29 12:32:23 -0400

    Fixed firebase sign up method and sign in methods

--- desktop/Unity/Assets/Firebase/Firebase.cs
+++ desktop/Unity/Assets/Firebase/Firebase.cs
@@ -8,7 +8,7 @@ public class Firebase
     private static Firebase UniqueInstance;
     static string FSApiURL = "https://firestore.googleapis.com/v1/";
     static string ProjectID = "foot-leprechauns";
-    string FSAuthURL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
+    string FSAuthURL = "https://identitytoolkit.googleapis.com/v1/accounts";
     string FSBaseURL = $"{FSApiURL}projects/{ProjectID}/databases/(default)/documents/users/";
     string CurrUserId;
     string API_KEY = System.IO.File.ReadAllText("Assets/Firebase/apikey.0");
@@ -60,7 +60,7 @@ public class Firebase
             req.password = userPassword;
 
             //Make HTTP Request
-            RestClient.Post($"{this.FSAuthURL}{this.API_KEY}", req).Then(res =>
+            RestClient.Post($"{this.FSAuthURL}:signInWithPassword?key={this.API_KEY}", req).Then(res =>
             {
                 SignInSuccessRes response = JsonConvert.DeserializeObject<SignInSuccessRes>(res.Text);
                 SignInRes fullResponse = new SignInRes();
@@ -79,6 +79,38 @@ public class Firebase
                 SignInRes fullResponse = new SignInRes();
                 fullResponse.Success = false;
                 callback(fullResponse);
+            });
+    }
+
+    public delegate void GetSignUpResCallback(SignUpRes signUpRes);
+    /// <summary>
+    /// Retrieves a user from the Firebase Database, given their id
+    /// </summary>
+    /// <param name="userId"> Id of the user that we are looking for </param>
+    /// <param name="callback"> What to do after the user is downloaded successfully </param>
+    public void SignUp(string userEmail, string userPassword, GetSignUpResCallback callback)
+    {
+            //Create request body
+            SignInReq req = new SignInReq();
+            req.email = userEmail;
+            req.password = userPassword;
+
+            //Make HTTP Request
+            RestClient.Post($"{this.FSAuthURL}:signUp?key={this.API_KEY}", req).Then(res =>
+            {
+                SignUpSuccessRes successResponse = JsonConvert.DeserializeObject<SignUpSuccessRes>(res.Text);
+                SignUpRes response = new SignUpRes();
+                response.Success = true;
+                response.Uid = successResponse.localId;
+                response.Email = successResponse.email;
+                callback(response);
+            }).Catch(err=>{
+                var error = err as RequestException;
+                SignUpErrorBody res = JsonConvert.DeserializeObject<SignUpErrorBody>(error.Response);
+                SignUpRes response = new SignUpRes();
+                response.Success = false;
+                response.ErrorMessage = res.error.message;
+                callback(response);
             });
     }
 }

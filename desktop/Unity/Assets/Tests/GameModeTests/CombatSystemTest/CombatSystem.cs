65e030b03a42c31d02b9327af2da70298c5f3acf
jsa5664 <47936036+jsa5664@users.noreply.github.com>
2021-11-29 17:41:04 -0500

    Added unit testing

--- desktop/Unity/Assets/Tests/GameModeTests/CombatSystemTest/CombatSystem.cs
+++ desktop/Unity/Assets/Tests/GameModeTests/CombatSystemTest/CombatSystem.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using CombatSystemComponent;
 
-//Tyler Anderson; Unit Test
-public class CombatSystemTest
-{
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
+//Jake Aldridge; Unit Test
+public class CombatSystemTest2
+{ 
     [UnityTest]
-    public IEnumerator CombatSystemTestWithEnumeratorPasses()
+    public IEnumerator CombatSystemTest2WithEnumeratorPasses()
     {
         CombatSystem combatSystem = new CombatSystem();
+        Timer timer = new Timer(1000);
 
-        yield return null;
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        Assert.AreEqual(timer.getMaxTime, 1000); 
+        Assert.AreEqual(CharacterBase.gravity, new Vector3(0, -9.8f, 0));
 
         float strength = 1;
         float defense = 1000;
@@ -25,5 +27,7 @@ public class CombatSystemTest
             "damage calculation test is less than or equal to zero");
 
         Assert.AreEqual("Hello from Component CombatSystem", combatSystem.Hello());
+
+        yield return null;
     }
 }
